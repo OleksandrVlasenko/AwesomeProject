1074c8ebbd6313a8f0999ba48008df9ed6bbce2d
OleksandrVlasenko <sania.rapsik@gmail.com>
2023-06-03 02:04:20 +0300

    registration

---
+++ src/Screens/PostsScreen.jsx
@@ -0,0 +1,11 @@
+import LoginScreen from "../Components/LoginScreen";
+import RegistrationScreen from "../Components/RegistrationScreen";
+
+export default function PostsScreen() {
+	return (
+		<>
+			<LoginScreen />
+			<RegistrationScreen />
+		</>
+	);
+}
